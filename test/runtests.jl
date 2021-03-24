1383420ddb08ef4acfbd30165b0a26ea2a4b6bba
roflmaostc <fxw+git@mailbox.org>
2021-03-24 09:51:36 +0100

    Add more runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
 using Random, Test
 using FourierTools
 
-@testset "Test resample_by_FFT method" begin
+@testset "Test resampling  methods" begin
     @testset "Test that upsample and downsample is reversible" begin
         Random.seed!(42)
-        for dim = 1:5
+        for dim = 1:4
             for _ in 1:4
                 s_small = ntuple(_ -> rand(1:13), dim)
                 s_large = ntuple(i -> max.(s_small[i], rand(10:16)), dim)
@@ -20,4 +20,35 @@ using FourierTools
 
     end
 
+
+    @testset "Tests that resample_by_FFT is purely real" begin
+        function test_real(s_1, s_2)
+            x = randn(Float32, (s_1))
+            y = resample_by_FFT(x, s_2, take_real=false)
+            @test all(( imag.(y) .+ 1 .≈ 1))
+        end
+
+        Random.seed!(42)
+        for dim = 1:4
+            for _ in 1:5
+                s_1 = ntuple(_ -> rand(1:13), dim)
+                s_2 = ntuple(i -> rand(1:13), dim)
+                test_real(s_1, s_2)
+            end
+        end
+            
+        test_real((4, 4),(6, 6))
+        test_real((4, 4),(6, 7))
+        test_real((4, 4),(9, 9))
+        test_real((4, 5),(9, 9))
+        test_real((4, 5),(9, 8))
+        test_real((8, 8),(6, 7))
+        test_real((8, 8),(6, 5))
+        test_real((8, 8),(4, 5))
+        test_real((9, 9),(4, 5))
+        test_real((9, 9),(4, 5))
+        test_real((9, 9),(7, 8))
+        test_real((9, 9),(6, 5))
+
+    end
 end
