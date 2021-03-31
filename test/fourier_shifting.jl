17ecf513f98fd1bcc6e787c6776ed9a96ad6e555
roflmaostc <fxw+git@mailbox.org>
2021-03-31 17:39:22 +0200

    More tests to check if sum is equal

--- test/fourier_shifting.jl
+++ test/fourier_shifting.jl
@@ -16,6 +16,8 @@ Random.seed!(42)
         @test FourierTools.shift!(copy(x), s) ≈ circshift(x, s) 
         s = (3,2,1)
         @test FourierTools.shift!(copy(x), s) ≈ circshift(x, s) 
+        
+        @test sum(x) ≈ sum(FourierTools.shift!(copy(x), s))
     end
 
     @testset "Half integer shifts" begin
@@ -25,17 +27,23 @@ Random.seed!(42)
         
         s = [0.5]
         @test FourierTools.shift!(copy(x), s) ≈ FourierTools.shift!(copy(xc), s)
-        @test FourierTools.shift!(copy(x), s) ≈ 0.5 .* ones(4)
+        @test sum(x) ≈ sum(FourierTools.shift!(copy(x), s))
+        
+        xc = [0.0, 1im, 0.0, 1im]
+        @test FourierTools.shift!(copy(xc), s) ≈ 1im .* 0.5 .* ones(4)
+        @test sum(xc) ≈ sum(FourierTools.shift!(copy(xc), s))
     end
 
 
     @testset "Random shifts consistency between both methods" begin
         x = randn((11, 12, 13))
         s = randn((3,)) .* 10
+        @test sum(x) ≈ sum(FourierTools.shift!(copy(x), s))
         @test FourierTools.shift!(copy(x), s) ≈ FourierTools.shift!(copy(x) .+ 0im, s)
         x = randn((11, 12, 13))
         s = randn((3,)) .* 10
         @test FourierTools.shift!(copy(x), s) ≈ FourierTools.shift!(copy(x) .+ 0im, s)
+        @test sum(x) ≈ sum(FourierTools.shift!(copy(x), s))
     end
 
 end
