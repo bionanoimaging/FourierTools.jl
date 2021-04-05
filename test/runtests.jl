d6ba0b14b650bde399d38883f865e02c6841cab2
roflmaostc <fxw+git@mailbox.org>
2021-04-05 18:44:28 +0200

    Update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,13 @@
 using Random, Test, FFTW
 using FourierTools
-
+using TestImages, ImageTransformations
 
 Random.seed!(42)
 
 include("fourier_shifting.jl")
 include("utils.jl")
 include("resampling_tests.jl")
+include("fourier_shear.jl")
+include("fourier_rotate.jl")
 
 return
