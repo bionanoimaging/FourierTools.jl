b2774af00fd48e84a4174e5483f11465756b6838
rheintzmann <heintzmann@gmail.com>
2021-08-05 09:02:31 +0200

    added the "using NDTools" to fix the CZT tests

--- test/czt.jl
+++ test/czt.jl
@@ -1,4 +1,6 @@
-@testset "CZT" begin
+using NDTools # this is needed for the select_region! function below.
+
+@testset "chirp z-transformation" begin
     @testset "czt" begin
         x = randn(ComplexF32, (5,6,7))
         y = randn(ComplexF32, (5,6))
@@ -6,7 +8,7 @@
         @test ≈(czt(x, zoom), ft(x),rtol=1e-4)         
         @test ≈(czt(y, zoom), ft(y),rtol=1e-5)
         zoom = (2.0,2.0)
-        @test ≈(czt(y,zoom),  select_region(upsample2(ft(y), fix_center=true),new_size=size(y)), rtol=1e-5)
+        @test ≈(czt(y,zoom),  select_region!(upsample2(ft(y), fix_center=true),new_size=size(y)), rtol=1e-5)
         # @vt czt(y,zoom)  select_region(upsample2(ft(y), fix_center=true), new_size=size(y))
     end
 end
