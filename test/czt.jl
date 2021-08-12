bfe9d714fc225c6bbb6210ec0561aaabb8260f33
rheintzmann <heintzmann@gmail.com>
2021-08-12 11:38:37 +0200

    added test for iczt

--- test/czt.jl
+++ test/czt.jl
@@ -7,6 +7,7 @@ using NDTools # this is needed for the select_region! function below.
         zoom = (1.0,1.0,1.0)
         @test ≈(czt(x, zoom), ft(x),rtol=1e-4)         
         @test ≈(czt(y, zoom), ft(y),rtol=1e-5)
+        @test ≈(iczt(czt(y,zoom),zoom),  y, rtol=1e-5)
         zoom = (2.0,2.0)
         @test ≈(czt(y,zoom),  select_region(upsample2(ft(y), fix_center=true),new_size=size(y)), rtol=1e-5)
         # @vt czt(y,zoom)  select_region(upsample2(ft(y), fix_center=true), new_size=size(y))
