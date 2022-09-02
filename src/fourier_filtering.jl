3b5f9d0f5b17549cb6f607039a514e07b5f199b1
RainerHeintzmann <heintzmann@gmail.com>
2022-09-02 12:31:03 +0200

    bug fixes and added the possibility to specify dims

--- src/fourier_filtering.jl
+++ src/fourier_filtering.jl
@@ -2,71 +2,214 @@ export fourier_filter!, fourier_filter
 export filter_gaussian # , filter_gaussian2
 
 """
+    fourier_filter!(arr::AbstractArray, fct=window_gaussian; kwargs...)
+    
+filters an array by multiplication in Fourierspace. This version uses in-place Fourier-transforms and multiplication whereever possible.
+The filter window function is assumed to be separable. Depending on the array type either full-complex or real-to-complex FFTs will be used.
+Note that some array types cannot directly be processed due to the type inference mechanism, in which case you should transform such types to
+a standard array type (usually via calling "collect").
+High-pass filters can be used by exchanging the border_in and border_out argument values.
+
+#Arguments
++`arr`:     the array to filter
++`fct`:     the separable window function to use. The function has an ND-array size as the first parameter but wil be called with only one non-singleton dimension at a time.
++`kwargs...`:   keyword arguments which will be passed to fct. These typically are `border_in=0.8` and `border_out=1.0` for the window-functions as defined in the `IndexFunArrays.jl` toolbox.
+
+#Example
+```jdoctest
+julia> arr = zeros(10,10); arr[3,4]=1.0;
+
+julia> fourier_filter!(arr)
+10×10 Matrix{Float64}:
+ -0.00747645   0.00747645  -0.00747645  -0.07899   -0.00747645  …  -0.00747645   0.00747645  -0.00747645   0.00747645
+  0.00747645  -0.00747645   0.00747645   0.07899    0.00747645      0.00747645  -0.00747645   0.00747645  -0.00747645
+  0.07899     -0.07899      0.07899      0.834544   0.07899         0.07899     -0.07899      0.07899     -0.07899
+  0.00747645  -0.00747645   0.00747645   0.07899    0.00747645      0.00747645  -0.00747645   0.00747645  -0.00747645
+ -0.00747645   0.00747645  -0.00747645  -0.07899   -0.00747645     -0.00747645   0.00747645  -0.00747645   0.00747645
+  0.00747645  -0.00747645   0.00747645   0.07899    0.00747645  …   0.00747645  -0.00747645   0.00747645  -0.00747645
+ -0.00747645   0.00747645  -0.00747645  -0.07899   -0.00747645     -0.00747645   0.00747645  -0.00747645   0.00747645
+  0.00747645  -0.00747645   0.00747645   0.07899    0.00747645      0.00747645  -0.00747645   0.00747645  -0.00747645
+ -0.00747645   0.00747645  -0.00747645  -0.07899   -0.00747645     -0.00747645   0.00747645  -0.00747645   0.00747645
+  0.00747645  -0.00747645   0.00747645   0.07899    0.00747645      0.00747645  -0.00747645   0.00747645  -0.00747645
+```
 """
-function fourier_filter!(arr::AbstractArray{<:Complex, N}, fct=window_gaussian; border_in=0.0, border_out=1.0) where {N}
-    return fourier_filter_by_1D_FT!(arr, fct; border_in=border_in, border_out=border_out)
+function fourier_filter!(arr::AbstractArray{<:Complex, N}, fct=window_gaussian; kwargs...) where {N}
+    return fourier_filter_by_1D_FT!(arr, fct; kwargs...)
 end
 
 function fourier_filter!(arr::AbstractArray{<:Int, N}, fct=window_gaussian; kwargs...) where {N}
     throw(ArgumentError("FFTW.jl does not accept AbstractArrays{<:Int, N}. Convert array to a Real/Complex."))
 end
 
-function fourier_filter!(arr::AbstractArray{<:Real, N}, fct=window_gaussian; border_in=0.0, border_out=1.0) where {N}
-    return fourier_filter_by_1D_RFT!(arr, fct;  border_in=border_in, border_out=border_out)
+function fourier_filter!(arr::AbstractArray{<:Real, N}, fct=window_gaussian; kwargs...) where {N}
+    return fourier_filter_by_1D_RFT!(arr, fct;  kwargs...)
+end
+
+function fourier_filter!(arr::AbstractArray{<:Complex, N}, wins::AbstractVector; transform_win=true, kwargs...) where {N}
+    return fourier_filter_by_1D_FT!(arr, wins; transform_win=transform_win, kwargs...)
+end
+
+function fourier_filter!(arr::AbstractArray{<:Real, N}, wins::AbstractVector; transform_win=true, kwargs...) where {N}
+    return fourier_filter_by_1D_RFT!(arr, fct; transform_win=transform_win, kwargs...)
 end
 
 """
-    fourier_filter(arr, limits)
+fourier_filter(arr::AbstractArray, fct=window_gaussian; kwargs...)
+    
+filters an array by multiplication in Fourierspace. This version uses in-place Fourier-transforms and multiplication whereever possible.
+The filter window function is assumed to be separable. Depending on the array type either full-complex or real-to-complex FFTs will be used.
+Note that some array types cannot directly be processed due to the type inference mechanism, in which case you should transform such types to
+a standard array type (usually via calling "collect").
+High-pass filters can be used by exchanging the border_in and border_out argument values.
 
-Returning a fourier_filtered array.
-See [`fourier_filter!`](@ref fourier_filter!) for more details
+#Arguments
++`arr`:     the array to filter
++`fct`:     the separable window function to use. The function has an ND-array size as the first parameter but wil be called with only one non-singleton dimension at a time.
++`kwargs...`:   keyword arguments which will be passed to fct. These typically are `border_in=0.8` and `border_out=1.0` for the window-functions as defined in the `IndexFunArrays.jl` toolbox.
+#Example
+```jdoctest
+julia> using IndexFunArrays
+
+julia> arr = zeros(10,10); arr[3,4]=1.0;
+
+julia> fourier_filter(arr, window_hanning; border_in=0.5, border_out=1.0)
+10×10 Matrix{Float64}:
+ -0.00528825    0.0132184    -0.0217829    -0.0994384    …  -0.00528825    0.00102161    0.000100793   0.00102161
+  0.00896055   -0.0223976     0.0369096     0.168491         0.00896055   -0.00173105   -0.000170787  -0.00173105
+  0.0317295    -0.0793102     0.130698      0.59663          0.0317295    -0.00612969   -0.00060476   -0.00612969
+  0.00896055   -0.0223976     0.0369096     0.168491         0.00896055   -0.00173105   -0.000170787  -0.00173105
+ -0.00528825    0.0132184    -0.0217829    -0.0994384       -0.00528825    0.00102161    0.000100793   0.00102161
+  0.00186562   -0.00466326    0.00768472    0.0350805    …   0.00186562   -0.000360412  -3.55585e-5   -0.000360412
+ -1.38778e-18   1.11022e-17   5.55112e-18  -2.22045e-17      0.0           1.73472e-19  -4.29344e-18  -6.41848e-18
+ -0.000499354   0.00124817   -0.0020569    -0.00938969      -0.000499354   9.64682e-5    9.51763e-6    9.64682e-5
+ -5.55112e-18   0.0           1.11022e-17   8.88178e-17     -5.55112e-18  -1.21431e-17  -2.47198e-18  -2.08167e-18
+  0.00186562   -0.00466326    0.00768472    0.0350805        0.00186562   -0.000360412  -3.55585e-5   -0.000360412```
+
 """
-function fourier_filter(arr, fct=window_gaussian;  border_in=0.0, border_out=1.0)
-    return fourier_filter!(copy(arr), fct; border_in=border_in, border_out=border_out)
+function fourier_filter(arr, fct=window_gaussian;  kwargs...)
+    return fourier_filter!(copy(arr), fct; kwargs...)
 end
 
-function fourier_filter_by_1D_FT!(arr::TA, fct=window_gaussian; border_in=0.0, border_out=1.0, dims=(1:ndims(arr))) where {N, TA <: AbstractArray{<:Complex, N}}
-    # iterates of the dimension d using the corresponding shift
-    sz = size(arr)
-    for d in dims #(d, limit) in pairs(limits)
-        w = TA(ifftshift(fct(real(eltype(arr)), select_sizes(arr,d), border_in=border_in, border_out=border_out)))
-        #w = TA(collect(ifftshift(fct(real(eltype(arr)), select_sizes(arr,d), border_in=border_in, border_out=border_out))))
-        # go to fourier space and apply w
-        fft!(arr, d)
-        arr .*= w
+function fourier_filter_by_1D_FT!(arr::TA, wins::AbstractVector; transform_win=false, dims=(1:ndims(arr))) where {N, TA <: AbstractArray{<:Complex, N}}
+    if isempty(dims)
+        return arr
     end
-
+    # iterates of the dimension d using the corresponding shift
+    for d in dims #(d, limit) in pairs(limits)
+        # go to fourier space and apply window
+        fft!(arr, d)
+        arr .*= let 
+            if transform_win
+                fft(wins[d], d)
+            else
+                wins[d]
+            end        
+        end
+    end
     ifft!(arr, dims)
     return arr
 end
 
-# the idea is the following:
-# rfft(x, 1) -> exp shift -> fft(x, 2) -> exp shift ->  fft(x, 3) -> exp shift -> ifft(x, [2,3]) -> irfft(x, 1)
-# So once we did a rft to shift something we can call the routine for complex arrays to shift
-function fourier_filter_by_1D_RFT!(arr::TA, fct=window_gaussian;  border_in=0.0, border_out=1.0) where {T<:Real, N, TA<:AbstractArray{T, N}}
-    sz = size(arr)
-    for d = 1:length(sz) #(d, limit) in pairs(limits)
-        p = plan_rfft(arr, d)
-
-        arr_ft = p * arr
-        w = TA(fct(real(eltype(arr)), select_sizes(arr_ft,d), offset=CtrRFFT, scale=2 ./size(arr,d), border_in=border_in, border_out=border_out))
-        # w = TA(collect(fct(real(eltype(arr)), select_sizes(arr_ft,d), offset=CtrRFFT, scale=2 ./size(arr,d), border_in=border_in, border_out=border_out)))
-        arr_ft .*= w
-        # since we now did a single rfft dim, we can switch to the complex routine
-        # new_limits = ntuple(i -> i ≤ d ? 0 : limits[i], N)
-         # workaround to mimic in-place rfft
-        fourier_filter_by_1D_FT!(arr_ft;  border_in=border_in, border_out=border_out, dims=(2:ndims(arr_ft)))
-        # go back to real space now and return because shift_by_1D_FT processed
-        # the other dimensions already
-        mul!(arr, inv(p), arr_ft)
-        return arr # this breaks the for loop and finishes the algorithm
+function fourier_filter_by_1D_FT!(arr::TA, fct=window_gaussian; dims=(1:ndims(arr)), transform_win=false, kwargs...) where {N, TA <: AbstractArray{<:Complex, N}}
+    if isempty(dims)
+        return arr
     end
+    # iterates of the dimension d using the corresponding shift
+    TR = real_arr_type(TA)
+    wins = Vector{TR}(undef, N)
+    # only calculate the necessary windows
+    for d in dims 
+        # these will possibly be transformed later
+        wins[d] = TR(ifftshift(fct(real(eltype(arr)), select_sizes(arr, d); kwargs...)))
+    end
+    return fourier_filter_by_1D_FT!(arr::TA, wins; transform_win=transform_win, dims=dims)
+end
+
+function fourier_filter_by_1D_RFT!(arr::TA, wins::AbstractVector; dims=(1:ndims(arr)), transform_win=false, kwargs...) where {T<:Real, N, TA<:AbstractArray{T, N}}
+    if isempty(dims)
+        return arr
+    end
+    d = dims[1] 
+    p = plan_rfft(arr, d)
+
+    arr_ft = p * arr
+    arr_ft .*= let 
+        if transform_win
+            pw = plan_rfft(wins[d], d)
+            pw * wins[d]
+        else
+            wins[d]
+        end
+    end
+    # since we now did a single rfft dim, we can switch to the complex routine
+    # new_limits = ntuple(i -> i ≤ d ? 0 : limits[i], N)
+    # workaround to mimic in-place rfft
+    fourier_filter_by_1D_FT!(arr_ft, wins; dims=dims[2:end], transform_win=transform_win, kwargs...)
+    # go back to real space now and return because shift_by_1D_FT processed
+    # the other dimensions already
+    mul!(arr, inv(p), arr_ft)
     return arr
 end
 
-function filter_gaussian(arr, sigma=eltype(arr)(1))
-    sigma_k = 2 ./ (pi .* sigma) 
-    fourier_filter(arr, border_out=sigma_k)
+# transforms the first dim as rft and then hands over to the fft-based routines.
+function fourier_filter_by_1D_RFT!(arr::TA, fct=window_gaussian; dims=(1:ndims(arr)), transform_win=false, kwargs...) where {T<:Real, N, TA<:AbstractArray{T, N}}
+    if isempty(dims)
+        return arr
+    end
+    TR = real_arr_type(TA)
+    d = dims[1]
+    p = plan_rfft(arr, d)
+    arr_ft = p * arr
+    win = TR(fct(real(eltype(arr)), select_sizes(arr_ft,d), offset=CtrRFFT, scale=2 ./size(arr,d); kwargs...))
+    if transform_win
+        pw = plan_rfft(win, d)
+        win = pw*win
+    end
+    arr_ft .*= win
+    fourier_filter_by_1D_FT!(arr_ft, fct;  dims=dims[2:end], transform_win=transform_win, kwargs...)
+    # go back to real space now and return because shift_by_1D_FT processed
+    # the other dimensions already
+    mul!(arr, inv(p), arr_ft)
+    return arr # this breaks the for loop and finishes the algorithm
+end
+
+"""
+    filter_gaussian(arr, sigma=eltype(arr)(1); border_in=(real(eltype(arr)))(0), border_out=(real(eltype(arr))).(2 ./ (pi .* sigma)), kwargs...)
+
+performs Gaussian filtering by multiplying a Gaussian function in Fourier space.
+Note that this is not identical to the Fourier-transform of a real-space Gaussian, especially for small kernel sizes and small arrays.
+See also `filter_gaussian!()` and `fourier_filter()`.
+
+#Arguments
++`arr`:     the array to filter
++`sigma`:     the real-space standard deviation to filter with. From this the Fourier-space standard deviation will be calculated. 
++`kwargs...`:   additional arguments to be passed to `window_gaussian`, which is the underlying function from `IndexFunArray.jl`. This can be useful to create Fourier-shifted (Gabor-) filtering.
+"""
+function filter_gaussian(arr, sigma=eltype(arr)(1); real_space_kernel=false, border_in=(real(eltype(arr)))(0), border_out=(real(eltype(arr))).(2 ./ (pi .* sigma)), kwargs...)
+    filter_gaussian!(copy(arr), sigma; real_space_kernel=real_space_kernel, border_in=border_in, border_out=border_out, kwargs...)
+end
+
+"""
+    filter_gaussian!(arr, sigma=eltype(arr)(1); border_in=(real(eltype(arr)))(0), border_out=(real(eltype(arr))).(2 ./ (pi .* sigma)), kwargs...)
+
+performs in-place Gaussian filtering by multiplying a Gaussian function in Fourier space.
+Note that this is not identical to the Fourier-transform of a real-space Gaussian, especially for small kernel sizes and small arrays.
+See also `filter_gaussian()` adn `fourier_filter!()`.
+
+#Arguments
++`arr`:     the array to filter
++`sigma`:     the real-space standard deviation to filter with. From this the Fourier-space standard deviation will be calculated. 
++`kwargs...`:   additional arguments to be passed to `window_gaussian`, which is the underlying function from `IndexFunArray.jl`. This can be useful to create Fourier-shifted (Gabor-) filtering.
+"""
+function filter_gaussian!(arr, sigma=eltype(arr)(1); real_space_kernel=false, border_in=(real(eltype(arr)))(0), border_out=(real(eltype(arr))).(2 ./ (pi .* sigma)), kwargs...)
+    if real_space_kernel
+        mysum = sum(arr)
+        fourier_filter!(arr, gaussian; transform_win=true, sigma=sigma, kwargs...)
+        arr .*= (mysum/sum(arr))
+        return arr
+    else
+        return fourier_filter!(arr; border_in=border_in, border_out=border_out, kwargs...)
+    end
 end
 
 # Suggested code by Felix. But it is not as fast as the code above:
---
+++ test/fourier_filtering.jl
@@ -0,0 +1,32 @@
+Random.seed!(42)
+
+@testset "Fourier filtering" begin
+
+    @testset "Gaussian filter complex" begin
+        sz = (21, 22)
+        x = randn(ComplexF32, sz)
+        sigma = (1.1,2.2)
+        gf = filter_gaussian(x, sigma)
+        # Note that this is not the same, since one kernel is generated in real space and one in Fourier space!
+        # with sizes around 10, the difference is huge!
+        k = gaussian(Float32, sz, sigma=sigma)
+        k = k./sum(k) # different than "normal".
+        gfc = conv_psf(x, k)
+        @test ≈(gf,gfc, rtol=1e-2) # it is realatively inaccurate due to the kernel being generated in different places
+        gfr = filter_gaussian(x, sigma, real_space_kernel=true)
+        @test ≈(gfr,gfc) # it can be debated how to best normalize a Gaussian filter
+    end
+
+    @testset "Gaussian filter real" begin
+        sz = (21, 22)
+        x = randn(Float32, sz)
+        sigma = (1.1,2.2)
+        gf = filter_gaussian(x, sigma)
+        # Note that this is not the same, since one kernel is generated in real space and one in Fourier space!
+        # with sizes around 10, the difference is huge!
+        k = gaussian(sz, sigma=sigma)
+        k = k./sum(k) # different than "normal".
+        gf2 = conv_psf(x, k)
+        @test ≈(gf,gf2, rtol=1e-2) # it is realatively inaccurate due to the kernel being generated in different places
+    end
+end
