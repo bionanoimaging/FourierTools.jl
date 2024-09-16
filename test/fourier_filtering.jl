9e9f30d3654a6bd5833ad5a45cc8fd1b5744cbdc
RainerHeintzmann <heintzmann@gmail.com>
2024-09-16 19:59:03 +0200

    test update

--- test/fourier_filtering.jl
+++ test/fourier_filtering.jl
@@ -15,7 +15,7 @@ Random.seed!(42)
         @test ≈(gf,gfc, rtol=1e-2) # it is realatively inaccurate due to the kernel being generated in different places
         gfr = filter_gaussian(x, sigma, real_space_kernel=true)
         @test ≈(gfr, gfc) # it can be debated how to best normalize a Gaussian filter
-        gfr = filter_gaussian(zeros(5).+1im, sigma, real_space_kernel=true)
+        gfr = filter_gaussian(zeros(5).+1im, (1.0,), real_space_kernel=true)
         @test ≈(gfr, zeros(5).+1im) # it can be debated how to best normalize a Gaussian filter
     end
 
@@ -30,8 +30,8 @@ Random.seed!(42)
         k = k./sum(k) # different than "normal".
         gf2 = conv_psf(x, k)
         @test ≈(gf,gf2, rtol=1e-2) # it is realatively inaccurate due to the kernel being generated in different places
-        gf2 = filter_gaussian(zeros(5), sigma, real_space_kernel=true)
-        @test ≈(gf2, zeros(5)) # it can be debated how to best normalize a Gaussian filter
+        gf2 = filter_gaussian(zeros(sz), sigma, real_space_kernel=true)
+        @test ≈(gf2, zeros(sz)) # it can be debated how to best normalize a Gaussian filter
     end
     @testset "Other filters" begin
         @test filter_hamming(FourierTools.delta(Float32, (3,)), border_in=0.0, border_out=1.0) ≈ [0.23,0.54, 0.23]
